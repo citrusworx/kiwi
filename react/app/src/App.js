ec96e75328aca830d87da3374a234dc90c495d7e
Drew Marshall <drewwinkles@gmail.com>
2024-08-10 22:29:55 -0500

    Initial commit

---
+++ react/app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NavTwoCol } from './mods/nav/nav'
+
+function App() {
+  return (
+    // This is where all of the app logic should sum up. I want the index.js file to have as minimal code as possible.
+    <div>
+      <NavTwoCol />
+    </div>
+  )
+}
+
+export default App
