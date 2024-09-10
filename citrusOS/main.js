305b2e4c357a5db06702a411d0a631328dc0e34b
Drew Marshall <43978216+stinklewinks@users.noreply.github.com>
2024-09-10 04:38:20 -0500

    Added basic menus for later development

---
+++ citrusOS/main.js
@@ -0,0 +1,32 @@
+const {app, BrowserWindow, Menu} = require('electron');
+const path = require('path');
+const newMenu = require('./util/menu/PageMenu');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+    win.loadFile('index.html');
+    const menu = Menu.buildFromTemplate(newMenu);
+    Menu.setApplicationMenu(menu);
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
--- typescript/app/src/install.ts
+++ typescript/app/src/install.ts
@@ -1,1 +1,15 @@
-export {}
+// This is an install script to set up settings for Kiwi Engine
+// This sets up initial configuration for databases, names, etc.
+class KiwiKernel {
+    private _name!: string;
+    private _version!: string;
+    private _description!: string;
+
+        
+    constructor(name: string, version: string, description: string) {
+        this._name = name;
+        this._version = version;
+        this._description = description;
+    }
+}
+export {KiwiKernel}
